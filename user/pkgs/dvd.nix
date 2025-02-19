a92e61d827ed2413088040e6f19e6cc8f70cbaa0
caio86 <caioluiz86@gmail.com>
2025-02-19 07:56:02 -0300

    feat(user.pkgs) add parameter validation for dvd and dvt scripts

--- user/pkgs/dvd.nix
+++ user/pkgs/dvd.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "dvd" ''
+  if [[ -z "$1" ]]; then
+    echo "Usage: dvd <template>"
+    exit 1
+  fi
+
   echo "use flake \"github:caio86/nixos?dir=templates/devshells/$1\"" >> .envrc
   direnv allow
 ''
--- user/pkgs/dvt.nix
+++ user/pkgs/dvt.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "dvt" ''
+  if [[ -z "$1" ]]; then
+    echo "Usage: dvt <template>"
+    exit 1
+  fi
+
   nix flake init -t github:caio86/nixos#$1
   direnv allow
 ''
