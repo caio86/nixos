d463eb9c488a6c617c62a098e23ea53f7ed3a6c9
caio86 <caioluiz86@gmail.com>
2025-02-22 02:30:58 -0300

    feat: create helper functions and add flake inputs to nixPath
    - create inspect-host function to use repl in a nixos config
    - put flake inputs inside nixPath

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -1,4 +1,20 @@
-{ self, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  self,
+  inputs,
+  adminUsername,
+  ...
+}:
+let
+  inherit (lib)
+    mapAttrs
+    mapAttrsToList
+    filterAttrs
+    isType
+    ;
+  configDir = "/home/${adminUsername}/.dotfiles";
+in
 {
   environment.systemPackages = [ pkgs.nvd ];
   # Useful for finding the exact config that built a generation
@@ -8,23 +24,54 @@
     config.allowUnfree = true;
   };
 
-  nix = {
-    settings = {
-      experimental-features = [
-        "flakes"
-        "nix-command"
-      ];
-      auto-optimise-store = true;
-    };
+  nix =
+    let
+      flakeInputs = filterAttrs (_: isType "flake") inputs;
+    in
+    {
+      # Populates the nix registry with all our flake inputs `nix registry list`
+      # Enables referencing flakes with short name in nix commands
+      # e.g. 'nix shell n#dnsutils' or 'nix shell hyprland#wlroots-hyprland'
+      registry = (mapAttrs (_: flake: { inherit flake; }) flakeInputs) // {
+        self.flake = self;
+        n.flake = inputs.nixpkgs;
+      };
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
+      # Add flake inputs to nix path. Enables loading flakes with <flake_name>
+      # like how <nixpkgs> can be referenced.
+      nixPath = mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+
+      settings = {
+        experimental-features = [
+          "flakes"
+          "nix-command"
+        ];
+        auto-optimise-store = true;
+      };
+
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 7d";
+      };
     };
-  };
 
   programs.zsh = {
+    interactiveShellInit = # bash
+      ''
+        inspect-host() {
+          if [ -z "$1" ]; then
+            echo "Usage: inspect-host <hostname>"
+            return 1
+          fi
+          nixos-rebuild repl --flake "${configDir}#$1"
+        }
+
+        build-package() {
+          NIXPKGS_ALLOW_UNFREE=1 nix build --impure --expr "with import <nixpkgs> {}; pkgs.callPackage $1 {}"
+        }
+      '';
+
     shellAliases = {
       system-size = "nix path-info --closure-size --human-readable /run/current-system";
     };
