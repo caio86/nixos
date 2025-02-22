8769941640c00ffa72c30b0d322bee1e7440f635
caio86 <caioluiz86@gmail.com>
2025-02-22 16:55:23 -0300

    add nix-community cachix and nix-on-droid cachix

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -81,6 +81,14 @@ in
           "nix-command"
         ];
         auto-optimise-store = true;
+        substituters = [
+          "https://nix-community.cachix.org"
+          "https://nix-on-droid.cachix.org"
+        ];
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
+        ];
       };
 
       gc = {
