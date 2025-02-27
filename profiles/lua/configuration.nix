805da250b4b9e1a2dd6a78113dcf5aded05e5164
caio86 <caioluiz86@gmail.com>
2025-02-26 21:02:07 -0300

    feat: set locale options in core/nix.nix module

--- profiles/lua/configuration.nix
+++ profiles/lua/configuration.nix
@@ -41,22 +41,6 @@
   networking.networkmanager.enable = true;
   networking.firewall.enable = false;
 
-  # Set your time zone.
-  time.timeZone = systemSettings.timezone;
-  # Select internationalisation properties.
-  i18n.defaultLocale = systemSettings.locale;
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = systemSettings.locale;
-    LC_IDENTIFICATION = systemSettings.locale;
-    LC_MEASUREMENT = systemSettings.locale;
-    LC_MONETARY = systemSettings.locale;
-    LC_NAME = systemSettings.locale;
-    LC_NUMERIC = systemSettings.locale;
-    LC_PAPER = systemSettings.locale;
-    LC_TELEPHONE = systemSettings.locale;
-    LC_TIME = systemSettings.locale;
-  };
-
   home-manager = {
 
     useGlobalPkgs = true;
