cdb074cee8227d7810f744e90307477b17950237
caio86 <caioluiz86@gmail.com>
2025-02-26 21:07:52 -0300

    refactor(lua): remove unnecessary configurations that is now in nixos modules
    - removed experimental-features
    - removed allow unFree
    - removed systemPackages not needed

--- profiles/lua/configuration.nix
+++ profiles/lua/configuration.nix
@@ -2,7 +2,6 @@
   inputs,
   self,
   pkgs,
-  systemSettings,
   userSettings,
   config,
   ...
@@ -20,15 +19,6 @@
     ../../system/wm/dwm.nix
     ../../system/style/stylix.nix
   ];
-  # Ensure nix flakes are enabled
-  nix.package = pkgs.nixFlakes;
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   nixpkgs.overlays = builtins.attrValues self.overlays;
 
@@ -52,10 +42,7 @@
     };
 
     users = {
-      caiol.imports = [
-        inputs.sops-nix.homeManagerModules.sops
-        ./home.nix
-      ];
+      caiol.imports = [ ./home.nix ];
     };
   };
 
@@ -70,17 +57,7 @@
     ];
   };
 
-  programs.zsh.enable = true;
-
   sops.secrets."caiol_password".neededForUsers = true;
-
-  environment.systemPackages = with pkgs; [
-    vim
-    neovim
-    wget
-    git
-    home-manager
-  ];
 
   system.stateVersion = "23.11";
 }
