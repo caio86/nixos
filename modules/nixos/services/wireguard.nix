37b75550c14369178a322ce15a884018000aad49
caio86 <caioluiz86@gmail.com>
2025-03-02 01:52:14 -0300

    feat: only execute wireguard module if atleast one interfaces is enabled

--- modules/nixos/services/wireguard.nix
+++ modules/nixos/services/wireguard.nix
@@ -16,49 +16,54 @@ let
   interfaces = config.${ns}.services.wireguard;
 in
 {
-  assertions = mkMerge (
-    mapAttrsToList (
-      interface: cfg:
-      asserts [
-        (config.sops.secrets."wg-${hostname}-key" != null)
-        "A private key for the host ${hostname} is missing"
-      ]
-    ) interfaces
-  );
+  config =
+    mkIf (builtins.any (enabled: enabled) (mapAttrsToList (interface: cfg: cfg.enable) interfaces))
+      {
 
-  sops.secrets."wg-${hostname}-key" = { };
+        assertions = mkMerge (
+          mapAttrsToList (
+            interface: cfg:
+            asserts [
+              (config.sops.secrets."wg-${hostname}-key" != null)
+              "A private key for the host ${hostname} is missing"
+            ]
+          ) interfaces
+        );
 
-  networking = mkMerge (
-    mapAttrsToList (
-      interface: cfg:
-      mkIf cfg.enable {
-        wg-quick.interfaces."wg-${interface}" = {
-          address = cfg.address;
-          listenPort = cfg.listenPort;
-          autostart = cfg.autoStart;
-          privateKeyFile = config.sops.secrets."wg-${hostname}-key".path;
-          dns = mkIf cfg.dns.enable [ cfg.dns.address ];
+        sops.secrets."wg-${hostname}-key" = { };
 
-          peers = cfg.peers;
-        };
+        networking = mkMerge (
+          mapAttrsToList (
+            interface: cfg:
+            mkIf cfg.enable {
+              wg-quick.interfaces."wg-${interface}" = {
+                address = cfg.address;
+                listenPort = cfg.listenPort;
+                autostart = cfg.autoStart;
+                privateKeyFile = config.sops.secrets."wg-${hostname}-key".path;
+                dns = mkIf cfg.dns.enable [ cfg.dns.address ];
 
-        firewall.allowedUDPPorts = optional (cfg.listenPort != null) cfg.listenPort;
+                peers = cfg.peers;
+              };
 
-        firewall.interfaces."wg-${interface}" = mkIf cfg.dns.host {
-          allowedTCPPorts = [ cfg.dns.port ];
-          allowedUDPPorts = [ cfg.dns.port ];
-        };
-      }
-    ) interfaces
-  );
+              firewall.allowedUDPPorts = optional (cfg.listenPort != null) cfg.listenPort;
 
-  programs.zsh.shellAliases = mkMerge (
-    mapAttrsToList (
-      interface: cfg:
-      mkIf (cfg.enable && !cfg.autoStart) {
-        "wg-${interface}-up" = "sudo systemctl start wg-quick-wg-${interface}";
-        "wg-${interface}-down" = "sudo systemctl stop wg-quick-wg-${interface}";
-      }
-    ) interfaces
-  );
+              firewall.interfaces."wg-${interface}" = mkIf cfg.dns.host {
+                allowedTCPPorts = [ cfg.dns.port ];
+                allowedUDPPorts = [ cfg.dns.port ];
+              };
+            }
+          ) interfaces
+        );
+
+        programs.zsh.shellAliases = mkMerge (
+          mapAttrsToList (
+            interface: cfg:
+            mkIf (cfg.enable && !cfg.autoStart) {
+              "wg-${interface}-up" = "sudo systemctl start wg-quick-wg-${interface}";
+              "wg-${interface}-down" = "sudo systemctl stop wg-quick-wg-${interface}";
+            }
+          ) interfaces
+        );
+      };
 }
