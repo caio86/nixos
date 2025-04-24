d237b60f388c83b6d5f9db8178be510871e1edac
caio86 <caioluiz86@gmail.com>
2025-04-24 16:54:30 -0300

    users: move users options from core.default.nix to core.users.nix

--- modules/nixos/core/default.nix
+++ modules/nixos/core/default.nix
@@ -2,60 +2,14 @@
   lib,
   pkgs,
   config,
-  username,
-  adminUsername,
   systemSettings,
   ...
 }:
 let
-  inherit (lib)
-    ns
-    mkOption
-    types
-    mkAliasOptionModule
-    ;
+  inherit (lib) ns;
 in
 {
-  imports = lib.${ns}.scanPaths ./. ++ [
-    (mkAliasOptionModule [ "userPackages" ] [
-      "users"
-      "users"
-      username
-      "packages"
-    ])
-
-    (mkAliasOptionModule [ "adminPackages" ] [
-      "users"
-      "users"
-      adminUsername
-      "packages"
-    ])
-
-  ];
-
-  options.${ns}.core = {
-
-    username = mkOption {
-      type = types.str;
-      readOnly = true;
-      default = username;
-      description = "The name of the primary user of the system";
-    };
-
-    adminUsername = mkOption {
-      type = types.str;
-      readOnly = true;
-      default = "caiol";
-      description = "The username of the admin user that exists on all hosts";
-    };
-
-    privilegedUser = mkOption {
-      type = types.bool;
-      default = true;
-      description = "If the primary user is in wheel group";
-    };
-
-  };
+  imports = lib.${ns}.scanPaths ./.;
 
   config = {
     programs.zsh.enable = true;
--- modules/nixos/core/users.nix
+++ modules/nixos/core/users.nix
@@ -7,40 +7,86 @@
   ...
 }:
 let
-  inherit (lib) ns optional;
+  inherit (lib)
+    ns
+    mkOption
+    types
+    optional
+    mkAliasOptionModule
+    ;
   inherit (config.${ns}.core) privilegedUser;
 in
 {
-  sops.secrets =
-    {
-      "${username}_password".neededForUsers = true;
-    }
-    // lib.optionalAttrs (username != adminUsername) {
+  imports = [
+    (mkAliasOptionModule [ "userPackages" ] [
+      "users"
+      "users"
+      username
+      "packages"
+    ])
 
-      "${adminUsername}_password".neededForUsers = true;
+    (mkAliasOptionModule [ "adminPackages" ] [
+      "users"
+      "users"
+      adminUsername
+      "packages"
+    ])
+  ];
+
+  options.${ns}.core = {
+    username = mkOption {
+      type = types.str;
+      readOnly = true;
+      default = username;
+      description = "The name of the primary user of the system";
     };
 
-  users = {
-    mutableUsers = true;
-    defaultUserShell = pkgs.zsh;
+    adminUsername = mkOption {
+      type = types.str;
+      readOnly = true;
+      default = "caiol";
+      description = "The username of the admin user that exists on all hosts";
+    };
+
+    privilegedUser = mkOption {
+      type = types.bool;
+      default = true;
+      description = "If the primary user is in wheel group";
+    };
+  };
+
+  config = {
+    sops.secrets =
+      {
+        "${username}_password".neededForUsers = true;
+      }
+      // lib.optionalAttrs (username != adminUsername) {
+
+        "${adminUsername}_password".neededForUsers = true;
+      };
+
     users = {
-      ${username} =
-        {
-          isNormalUser = true;
-          hashedPasswordFile = config.sops.secrets."${username}_password".path;
-          extraGroups = optional privilegedUser "wheel";
-        }
-        // lib.optionalAttrs (username != adminUsername) {
-          "${username}" = {
-            uid = 1;
-            isSystemUser = true;
-            useDefaultShell = true;
-            createHome = true;
-            home = "/home/${adminUsername}";
-            hashedPasswordFile = config.sops.secrets."${adminUsername}_password".path;
-            group = "wheel";
+      mutableUsers = true;
+      defaultUserShell = pkgs.zsh;
+      users = {
+        ${username} =
+          {
+            isNormalUser = true;
+            hashedPasswordFile = config.sops.secrets."${username}_password".path;
+            extraGroups = optional privilegedUser "wheel";
+          }
+          // lib.optionalAttrs (username != adminUsername) {
+            "${username}" = {
+              uid = 1;
+              isSystemUser = true;
+              useDefaultShell = true;
+              createHome = true;
+              home = "/home/${adminUsername}";
+              hashedPasswordFile = config.sops.secrets."${adminUsername}_password".path;
+              group = "wheel";
+            };
           };
-        };
+      };
     };
   };
 }
