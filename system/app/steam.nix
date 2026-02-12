52688daf6554da0c90d735f9c9c08fd1928d7727
caio86 <caioluiz86@gmail.com>
2026-02-11 21:43:04 -0300

    renamed opengl to graphics option for enabling 32bits

--- system/app/steam.nix
+++ system/app/steam.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  hardware.opengl.driSupport32Bit = true;
+  hardware.graphics.enable32Bit = true;
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
   environment.systemPackages = [ pkgs.steam ];
