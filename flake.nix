adf4a7bc4ed6240059fd2138077ee4cc92a64c89
caio86 <caioluiz86@gmail.com>
2026-04-22 18:43:30 -0300

    remove unused nixpkgs-new flake input

--- flake.nix
+++ flake.nix
@@ -57,7 +57,6 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs-new.url = "nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
