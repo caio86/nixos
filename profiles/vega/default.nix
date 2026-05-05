3d1caec7d749f5d2cde202e8da7a63f25095c2f4
caio86 <caioluiz86@gmail.com>
2026-05-05 19:11:00 -0300

    use dhcp on vega profile

--- profiles/vega/default.nix
+++ profiles/vega/default.nix
@@ -31,11 +31,6 @@
     '';
   };
 
-  networking.networkmanager.dns = "none";
-  networking.useDHCP = false;
-  networking.dhcpcd.enable = false;
-  networking.nameservers = [ "192.168.0.254" ];
-
   services.openssh = {
     enable = true;
     openFirewall = true;
